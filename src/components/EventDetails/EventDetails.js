9bed4c8b695376e262ca6fe2a72725d7a2c5f771
Matthew Harris <matthewharrisak@gmail.com>
2020-11-16 16:35:27 -0600

    bandcamp link is showing

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-// import Iframe from 'react-iframe'
+import Iframe from 'react-iframe'
 
 
 
@@ -12,7 +12,7 @@ class NewEventForm extends Component {
     // fires off funtions on pageload
     componentDidMount = () => {
       this.props.dispatch({ type: 'GOT_EVENTS'});
-      console.log(this.props.store.event.event);
+    
       
     } 
   render() {
@@ -21,10 +21,22 @@ class NewEventForm extends Component {
           
               <h1> this is where we'll add the display events form </h1>
               {this.props.store.event.event.map((event) =>{
-                return <div key={event.event_id}> {event.title} </div>
+                return <div key={event.event_id}> {event.title} {event.address}
+                {event.description} {event.date} {event.image_url} 
+
+                </div>
               })}
-              {JSON.stringify(this.props.store.event)}
+              {/* {JSON.stringify(this.props.store.event)} */}
          
+
+
+              <Iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=3651962689/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" 
+              seamless><a href="https://terminationdust.bandcamp.com/album/growing-down">
+                Growing Down by 
+              Termination Dust</a></Iframe>
+
+
+
       </div>
     );
   }
