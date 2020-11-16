573a268f03af726fb61adc75d3d7edf69f2f796a
Matthew Harris <matthewharrisak@gmail.com>
2020-11-16 13:57:31 -0600

    created new event form

--- src/components/NewEventForm/NewEventForm.js
+++ src/components/NewEventForm/NewEventForm.js
@@ -5,11 +5,51 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class NewEventForm extends Component {
   // this component doesn't do much to start, just renders some user info to the DOM
+
+    state={
+        newEvent:{
+            title: '',
+            address: '',
+            description: '',
+            date: '',
+            image_url: '',
+            bandcamp: '',
+         }
+    }
+
+    handleChange= (keyname, event) => {
+        event.preventDefault();
+        this.setState({
+          newMovie:{
+            ...this.state.newEvent,
+            [keyname]: event.target.value,
+        }});
+      }
+
   render() {
     return (
       <div>
           <form>
               <h1> this is where we'll add the display events form </h1>
+
+              <input  onChange={(event) => this.handleChange( 'title' , event)} 
+                    type="text" id="" placeholder='title'/>
+
+                <input  onChange={(event) => this.handleChange( 'address' , event)} 
+                    type="text" id="" placeholder='address'/>
+
+                <input  onChange={(event) => this.handleChange( 'description' , event)} 
+                                    type="text" id="" placeholder='description'/>
+
+                <input  onChange={(event) => this.handleChange( 'date' , event)} 
+                                    type="text" id="" placeholder='date'/>
+
+                <input  onChange={(event) => this.handleChange( 'image_url' , event)} 
+                                    type="text" id="" placeholder='image link'/>
+
+                <input  onChange={(event) => this.handleChange( 'bandcamp' , event)} 
+                                    type="text" id="" placeholder='bandcamp link'/>
+
           </form>
       </div>
     );
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import NewEventForm from '../NewEventForm/NewEventForm';
 
 class UserPage extends Component {
   // this component doesn't do much to start, just renders some user info to the DOM
   render() {
     return (
       <div>
-        <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
+        {/* <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
         <p>Your ID is: {this.props.store.user.id}</p>
         <LogOutButton className="log-in" />
         <button> this button will route you to the add new event form</button>
-        <h1> this is where we'll display user specific events </h1>
+        <h1> this is where we'll display user specific events </h1> */}
+
+        <NewEventForm/>
       </div>
     );
   }
