4b591b4370211f7132ccdbbf0ac6e45ce8553647
Matthew Harris <matthewharrisak@gmail.com>
2020-11-28 15:00:58 -0600

    added styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -33,14 +33,7 @@ const Nav = (props) => {
         {/* Show the link to the info page and the logout button if the user is logged in */}
         {props.store.user.id && (
           <>
-            <Link className="nav-link" to="/info">
-              Info Page
-            </Link>
-
-            {/* <Link className="nav-link" to='/ResponsiveForm'>
-              Add New Event!
-            </Link> */}
-
+          
             <ResponsiveForm className="nav-link" />
             <LogOutButton className="nav-link" />
 
