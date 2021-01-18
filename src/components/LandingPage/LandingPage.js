2eb9152ecd10a2b81cafa815cabafb809b1bfc8b
Jinno Redovan <dashpatterns@jinno.local>
2021-01-17 19:27:21 -0600

    Refactored EventItems to allow for more dynamic use. Added styled components to eventually ease the use of themes. Added useFormatDate hook for reusability

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -9,57 +9,43 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Grid from '@material-ui/core/Grid';
 
-class LandingPage extends Component {
- 
+import get from 'lodash/get';
+import { EventItem } from '../EventItem';
 
-  
-// On page load - this function will load content from the DB table "events"
-    componentDidMount = () => {
-    this.props.dispatch({ type: 'GOT_EVENTS'});
+class LandingPage extends Component {
+
+
+
+  // On page load - this function will load content from the DB table "events"
+  componentDidMount = () => {
+    this.props.dispatch({ type: 'GOT_EVENTS' });
   }
 
   // fires off on click event - routes user to "EventDetails" page where they view details for specific event. 
   // Payload - is tied to the "event_id" which is a property of our event object.
   moreInfo = (moreInfo) => {
-    this.props.history.push('/EventDetails');
-    this.props.dispatch({type: 'GET_DETAILS', payload: moreInfo});
-    console.log(moreInfo);
+    // this.props.history.push('/EventDetails');
+    // this.props.dispatch({ type: 'GET_DETAILS', payload: moreInfo });
+    // console.log(moreInfo);
   }
 
   render() {
-    
-    return (
-      
-      <div className="container">
-      <Grid>
- 
-      <h1 className='whatsHappening'> Upcoming Events</h1>
-     
-              {this.props.store.event.event.map((event) =>{
-                          return  <ListItem key={event.event_id} id='landingTable'> 
-                          
-                          <Grid item 
-                          spacing={3}
-                            container
-                            direction="row">
-                          <ListItemText primary= {event.title} />
-                          </Grid>
-                          
-                          <Grid>
-                          <ListItemText 
-                            primary= {event.date} />
-                          </Grid>
-                          
-                          <ListItem/>
-                          <button  className='landingButton'
-                            onClick={() => this.moreInfo(event)}>
-                            more info!!
-                            </button>
-                            </ListItem>       
-                })}
 
-      
-      </Grid>
+    return (
+
+      <div className="container">
+        <Grid>
+
+          <h1 className='whatsHappening'> Upcoming Events</h1>
+
+          {this.props.store.event.event.map((event) =>
+          (<EventItem
+            key={get(event, 'event_id', get(event, 'title'))}
+            event={event}
+            dispatch={this.props.dispatch} />))
+          }
+
+        </Grid>
       </div>
     );
   }
