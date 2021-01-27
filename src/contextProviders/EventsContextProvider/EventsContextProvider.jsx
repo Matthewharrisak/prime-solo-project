9dc3034922a2e08e7a625860dbc87b9b78cae6d3
Jinno <jredovan.dev@gmail.com>
2021-01-27 13:25:19 -0600

    adding comments and userEvents

--- src/contextProviders/EventsContextProvider/EventsContextProvider.jsx
+++ src/contextProviders/EventsContextProvider/EventsContextProvider.jsx
@@ -6,25 +6,48 @@ import { reducer } from './reducer';
 const EventContext = createContext(null);
 
 const DEFAULT_STATE = {
-    events: []
+    events: [],
+    userEvents: []
 }
+
+/**
+ * 
+ * @param {children} param
+ */
 
 export const EventsContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, DEFAULT_STATE);
 
     const events = useMemo(() => get(state, 'events'), [state]);
+    const userEvents = useMemo(() => get(state, 'userEvents'), [state])
 
     const getEvents = useCallback(async () => {
+        //! EVENT IDs ARE NOT RETURNED
         const response = await axios.get('/api/events');
         const { data } = response;
         dispatch({ type: 'setEvents', events: data });
-    }, [])
+    }, []);
+
+    const getUserEvents = useCallback(async userId => {
+        const response = await axios.get(`/api/events/userEvent/${userId}`);
+        const { data } = response;
+        dispatch({ type: 'setUserEvents', userEvents: data })
+    }, []);
+
+    const deleteUserEvent = useCallback(async eventId => {
+        // TODO prev iteration requires entire event. 
+        // TODO we should probably just send in the eventId?
+        await axios.delete(`/api/events/${eventId}`);
+        dispatch({ type: 'deleteUserEvent', eventId })
+    }, []);
 
     const value = {
         events,
+        userEvents,
 
         // actions
-        getEvents
+        getEvents,
+        getUserEvents
     };
 
     return <EventContext.Provider value={value}>{children}</EventContext.Provider>
